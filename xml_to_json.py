41f4042905cf29555d7f49c88aac6a14d6ad418c
nocibambi <nocibambi@gmail.com>
2018-03-19 23:17:57 +0100

    Collecting requirements

---
+++ xml_to_json.py
@@ -0,0 +1,16 @@
+# Challenge Problem
+
+We require fellows to work on a small challenge problem to assess problem solving and coding capabilities. Select a problem from the list below.  Ideally perform your analysis in a jupyter notebook.  Post the notebook on Github and submit your results.
+
+Some hints for hacking our challenge:
+* Ask yourself why would they have selected this problem for the challenge? What are some gotchas in this domain I should know about?
+* What is the highest level of accuracy that others have achieved with this dataset or similar problems / datasets ?
+* What types of visualizations will help me grasp the nature of the problem / data?
+* What feature engineering might help improve the signal?
+* Which modeling techniques are good at capturing the types of relationships I see in this data?
+* Now that I have a model, how can I be sure that I didn't introduce a bug in the code? If results are too good to be true, they probably are!
+* What are some of the weakness of the model and and how can the model be improved with additional work?
+
+The challenge problems will give you an idea of what is expected from successful candidates. We look for creative problem solving ability, basic coding proficiency (particularly in python) and a foundational understanding of machine learning theory and methods.
+
+https://dumps.wikimedia.org/enwiki/latest/
